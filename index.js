5e0c6e51535d66c2008495942b885e4025e72f8b
xiaohp <xiaohp1993@gmail.com>
2019-01-21 11:33:39 +0800

    code format

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const Koa = require('koa');
-const { ApolloServer, gql } = require('apollo-server-koa');
+const Koa = require('koa')
+const { ApolloServer, gql } = require('apollo-server-koa')
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
     hello: String
   }
-`;
+`
 
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
     hello: () => 'Hello world!',
   },
-};
+}
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers })
 
-const app = new Koa();
-server.applyMiddleware({ app });
+const app = new Koa()
+server.applyMiddleware({ app })
 
-const port = 3000;
-const host = 'localhost';
+const port = 3000
+const host = 'localhost'
 
 app.listen(port, host, () =>
-  console.log(`🚀 Server ready at http://${host}:${port}${server.graphqlPath}`),
-);
+  console.log(`🚀 Server ready at http://${host}:${port}${server.graphqlPath}`)
+)
